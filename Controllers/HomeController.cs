afd0ec32905aa4921a51909d9db3db643068bef3
Adam N <anoah1225@gmail.com>
2019-07-17 08:12:00 -0400

    Debugging

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,15 @@
-﻿using EmployeeManagement.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController
     {
-        private IEmployeeRepository _employeeRepository;
-
-        public HomeController(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
         public string Index()
         {
-            return _employeeRepository.GetEmployee(1).Name;
+            return "Hello from MVC";
         }
     }
 }
